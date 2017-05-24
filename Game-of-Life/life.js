22b487cde3e111b8b287027d389c4fa7644ef149
Mike Williams <makerdude9@gmail.com>
2017-05-24 10:27:56 -0400

    Test push
    This is a test push too see if my files are added to the remote repository correctly

--- Game-of-Life/life.js
+++ Game-of-Life/life.js
@@ -2,23 +2,25 @@
 function main() {
   
   // Global Variables
-  var rows = 50;
+  var rows = 100;
   var columns = 100;
   var gridIndex = new Array();
-  var $row = $("<div />", {
-    class: 'row'
+  var $lane = $("<div />", {
+    class: 'lane'
   });
   var $box = $("<div />", {
     class: 'box'
   });
+ 
+  
 
   // Generates the visual grid on the page
   function generateGrid() {
     for(var i = 0; i < columns; i++) {
-      $row.append($box.clone());
+      $lane.append($box.clone());
     }
     for(var j = 0; j < rows; j++) {
-      $("#wrapper").append($row.clone());
+      $("#wrapper").append($lane.clone());
     }
   }
 
@@ -35,8 +37,27 @@ function main() {
 
   var indexValues = createGridIndex();
   generateGrid();
+  
+  var htmlText = parseInt($('html').css('font-size'));
+  var boxWidth = $('.lane .box').css('width');
+  var boxHeight = $('.lane .box').css('height');
+  var laneWidth = parseInt($('.lane').css('width'));
+  var laneHeight = $('.lane').css('height');
+  //console.log(boxWidth);
+  //console.log(boxHeight);
+  //console.log(laneWidth);
+  //console.log(laneHeight);
+  //console.log(htmlText);
+  //boxWidth = laneWidth / (columns);
+  //boxHeight = 25*htmlText/rows;
+  //laneHeight = boxHeight;
+  $('.lane .box').css('width', boxWidth);
+  $('.lane .box').css('height', boxHeight);
+  $('.lane').css('height', laneHeight);
+  //console.log(boxWidth);
+  //console.log(boxHeight);
 
-  console.log(indexValues);
+  //console.log(indexValues);
 }
 
 $(document).ready(main);
